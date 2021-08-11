4fbfa2c2020a707e716e647f5a2238a2dfae36a4
Sankalpa Shrestha <88698739+Sankalpa830@users.noreply.github.com>
2021-08-11 11:08:33 +0545

    Updates

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('username');
-            $table->string('lastname');
-            $table->string('firstname');
+            $table->string('last_name');
+            $table->string('first_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
