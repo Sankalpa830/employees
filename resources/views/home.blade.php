74a77b550e785d458372a4c04d3a53484227cf8d
Sankalpa Shrestha <88698739+Sankalpa830@users.noreply.github.com>
2021-09-02 09:04:39 +0545

    Updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            
+        <div>
+            <h1>Welcome!!</h1>
         </div>
     </div>
 </div>
